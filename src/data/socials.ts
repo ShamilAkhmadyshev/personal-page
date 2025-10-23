db0d064feb59415991cfd510c205b7836c1bc859
shamilakhmadyshev <deker929@gmail.com>
2025-10-23 12:23:27 +0200

    Fix:social links

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -1,5 +1,5 @@
 export const SOCIALS = [
   { label: 'GitHub', href: 'https://github.com/ShamilAkhmadyshev', icon: 'github' },
-  { label: 'LinkedIn', href: 'https://www.linkedin.com/in/shamil', icon: 'linkedin' },
+  { label: 'LinkedIn', href: 'https://www.linkedin.com/in/shamilakh', icon: 'linkedin' },
   { label: 'Telegram', href: 'https://t.me/dekerdelrey', icon: 'telegram' },
 ] as const
